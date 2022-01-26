2bf40e5735d6d39f12bd4f9bd87bef1e31e97430
NaJeongHu <65152311+NaJeongHu@users.noreply.github.com>
2022-01-26 11:39:14 +0900

    Update 이분탐색_징검다리.cpp

--- 정후/프로그래머스/이분탐색_징검다리.cpp
+++ 정후/프로그래머스/이분탐색_징검다리.cpp
@@ -5,23 +5,32 @@
 using namespace std;
 
 int solution(int distance, vector<int> rocks, int n) {
-    int answer = 0, min_answer = 1000000000;
+    int answer = 0, min_answer = 0;
     int start = 0, end = distance;
 
     sort(rocks.begin(), rocks.end());
 
+    
     vector<int> distances;      // 바위 사이 거리
     distances.push_back(rocks.at(0));
     for (int i = 1; i < rocks.size(); i++)
         distances.push_back(rocks.at(i) - rocks.at(i - 1));
     distances.push_back(distance - rocks.at(rocks.size() - 1));
+    
 
     while (start <= end) {
         int mid = (start + end) / 2;
         int cnt = 0;    // 없앤 바위 개수
         int idx = 0;
+        int left = 0;
 
-        for (int i = 0; i <= rocks.size(); i++) {
+        for (int i = 0; i < rocks.size(); i++) {
+
+            /*
+            if (rocks[i] - left >= mid) left = rocks[i];
+            else cnt++;
+            */
+            
             if (idx != i) {
                 int temp = 0;
                 for (int j = idx; j <= i; j++) {
@@ -40,6 +49,7 @@ int solution(int distance, vector<int> rocks, int n) {
                     idx++;
                 }
             }
+            
         }
 
         if (cnt > n) {
@@ -47,7 +57,7 @@ int solution(int distance, vector<int> rocks, int n) {
         }
         else {
             answer = mid;
-            if (min_answer > answer) min_answer = answer;
+            if (min_answer < answer) min_answer = answer;
             start = mid + 1;
         }
     }
